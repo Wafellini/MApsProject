f631c31c99d2fd49cc9bbf360177398cf9b1de78
Feliks Czart <czart78@gmail.com>
2021-06-16 13:55:16 +0200

    satelite

--- app/src/main/java/com/example/mapsproject/activity/MapActivity.kt
+++ app/src/main/java/com/example/mapsproject/activity/MapActivity.kt
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 //import com.google.maps.example.R
 
@@ -32,17 +33,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
